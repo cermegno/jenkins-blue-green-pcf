69015e391125f8a91cc174c8d16b0fd0b3aa6c8d
Alberto Ramos <alberto.ramos@dell.com>
2019-05-18 18:29:27 +1000

    Update step1.py

--- step1.py
+++ step1.py
@@ -5,9 +5,10 @@ print r.get('foo')
 r.hmset('survey1',{'division':'ENT','state':'NSW','feedback':'hello world'})
 r.hmset('survey2',{'division':'COMM','state':'NSW','feedback':'great'})
 r.hmset('survey3',{'division':'ENT','state':'VIC','feedback':'awesome'})
-for eachsurvey in r.keys('new_survey*'):
-    response += "Division : " + r.hget(eachsurvey,'division') + "<br>"
-    response += "State    : " + r.hget(eachsurvey,'state') + "<br>"
-    response += "Feedback : " + r.hget(eachsurvey,'feedback') + "<br>"
+response = "Full Report\n"
+for eachsurvey in r.keys('survey*'):
+    response += "Division : " + r.hget(eachsurvey,'division') + "<br>\n"
+    response += "State    : " + r.hget(eachsurvey,'state') + "<br>\n"
+    response += "Feedback : " + r.hget(eachsurvey,'feedback') + "<br>\n"
     response += " ----------------------<br>"
 print response
