b7a6db4657883fe32fe7fd8c9cfb76e1fd6ec2c4
Alberto Ramos <alberto.ramos@dell.com>
2019-05-20 09:25:07 +1000

    Update bgchoose.sh

--- bgchoose.sh
+++ bgchoose.sh
@@ -1,7 +1,9 @@
 if [ -f "green" ]; then
     echo "We will push blue"
     mv green blue
+    sed -i 's/green/blue/g' manifest.yml
 elif [ -f "blue" ]; then
     echo "We will push green"
     mv blue green
+    sed -i 's/blue/green/g' manifest.yml
 fi
